090530bc35f2906794390cfe04d722e5b1ae4a74
LRyanX <821114264@qq.com>
2018-01-03 14:46:16 +0800

    add schedure

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,6 +1,6 @@
 class CoursesController < ApplicationController
 
-  before_action :student_logged_in, only: [:select, :quit, :selectd]
+  before_action :student_logged_in, only: [:select, :quit, :selectd, :schedule]
   before_action :teacher_logged_in, only: [:new, :create, :edit, :destroy, :update, :open, :close, :advselect]#add open by qiao
   before_action :logged_in, only: :index
   before_action :credit 
@@ -89,6 +89,226 @@ class CoursesController < ApplicationController
     @course=tmp
   end
 
+  def schedule
+
+    @section1=Array.new 
+    @section2=Array.new
+    @section3=Array.new
+    @section4=Array.new
+    @section5=Array.new
+    @section6=Array.new
+    @section7=Array.new
+    @section8=Array.new
+    @section9=Array.new
+    @section10=Array.new
+    @section11=Array.new
+
+    @courses=current_user.courses
+    @courses.each do |course|
+      week=course.course_time.split("(")[0]
+      start=course.course_time.split("(")[1].split("-")[0].to_i
+      end_c=course.course_time.split("(")[1].split("-")[1].split(")")[0].to_i
+
+	  inter=end_c-start+1
+      if start == 1
+		if inter == 1
+			@section1<<course
+		end
+		if inter == 2
+			@section1<<course
+			@section2<<course
+		end
+		if inter == 3
+			@section1<<course
+			@section2<<course
+			@section3<<course
+		end
+		if inter == 4
+			@section1<<course
+			@section2<<course
+			@section3<<course
+			@section4<<course
+		end
+
+      end
+      if start == 2
+		if inter == 1
+			@section2<<course
+		end
+		if inter == 2
+			@section2<<course
+			@section3<<course
+		end
+		if inter == 3
+			@section2<<course
+			@section3<<course
+			@section4<<course
+		end
+		if inter == 4
+			@section2<<course
+			@section3<<course
+			@section4<<course
+			@section5<<course
+		end
+      end
+      if start == 3
+		if inter == 1
+			@section3<<course
+		end
+		if inter == 2
+			@section3<<course
+			@section4<<course
+		end
+		if inter == 3
+			@section3<<course
+			@section4<<course
+			@section5<<course
+		end
+		if inter == 4
+			@section3<<course
+			@section4<<course
+			@section5<<course
+			@section6<<course
+		end
+      end
+      if start == 4
+		if inter == 1
+			@section4<<course
+		end
+		if inter == 2
+			@section4<<course
+			@section5<<course
+		end
+		if inter == 3
+			@section4<<course
+			@section5<<course
+			@section6<<course
+		end
+		if inter == 4
+			@section4<<course
+			@section5<<course
+			@section6<<course
+			@section7<<course
+		end
+      end
+      if start == 5
+		if inter == 1
+			@section5<<course
+		end
+		if inter == 2
+			@section5<<course
+			@section6<<course
+		end
+		if inter == 3
+			@section5<<course
+			@section6<<course
+			@section7<<course
+		end
+		if inter == 4
+			@section5<<course
+			@section6<<course
+			@section7<<course
+			@section8<<course
+		end
+      end
+      if start == 6
+		if inter == 1
+			@section6<<course
+		end
+		if inter == 2
+			@section6<<course
+			@section7<<course
+		end
+		if inter == 3
+			@section6<<course
+			@section7<<course
+			@section8<<course
+		end
+		if inter == 4
+			@section6<<course
+			@section7<<course
+			@section8<<course
+			@section9<<course
+		end
+      end
+      if start == 7
+		if inter == 1
+			@section7<<course
+		end
+		if inter == 2
+			@section7<<course
+			@section8<<course
+		end
+		if inter == 3
+			@section7<<course
+			@section8<<course
+			@section9<<course
+		end
+		if inter == 4
+			@section7<<course
+			@section8<<course
+			@section9<<course
+			@section10<<course
+		end
+      end
+      if start == 8
+		if inter == 1
+			@section8<<course
+		end
+		if inter == 2
+			@section8<<course
+			@section9<<course
+		end
+		if inter == 3
+			@section8<<course
+			@section9<<course
+			@section10<<course
+		end
+		if inter == 4
+			@section8<<course
+			@section9<<course
+			@section10<<course
+			@section11<<course
+		end
+      end
+      if start == 9
+		if inter == 1
+			@section9<<course
+		end
+		if inter == 2
+			@section9<<course
+			@section10<<course
+		end
+		if inter == 3
+			@section9<<course
+			@section10<<course
+			@section11<<course
+		end
+
+      end
+      if start == 10
+		if inter == 1
+			@section10<<course
+		end
+		if inter == 2
+			@section10<<course
+			@section11<<course
+		end
+
+      end
+      if start == 11
+		if inter == 1
+			@section11<<course
+		end
+      end
+
+
+    end
+    
+
+
+  end
+
   def select
     @course=Course.find_by_id(params[:id])
     current_user.courses<<@course
@@ -110,6 +330,7 @@ class CoursesController < ApplicationController
     current_user.sum_credit -= credit
 
     if @course.is_degree
+
        @course.is_degree = false
        @course.save(:validate => false)
        current_user.degree_credit -= credit
@@ -132,7 +353,6 @@ class CoursesController < ApplicationController
     @course.is_degree = true
     @course.save(:validate => false)
     
-
     current_user.save(:validate => false)
 
     flash={:suceess => "成功选择课程: #{@course.name}"}
--- config/routes.rb
+++ config/routes.rb
@@ -29,6 +29,7 @@ Rails.application.routes.draw do
     end
     collection do
       get :list
+      get :schedule
     end
   end
 
